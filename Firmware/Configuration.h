3f0351c3fbada4005b778fb5c9b1644d271c5444
wschadow <wolfgang.schadow@t-online.de>
2023-02-13 15:38:42 +0100

    pushes build no

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -27,8 +27,8 @@ extern PGM_P sPrinterName;
     #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION)
 #endif
 
-#define FW_COMMIT_NR 6528
-#define FW_COMMIT_HASH "a6702c00"
+#define FW_COMMIT_NR 6530
+#define FW_COMMIT_HASH "59456a8c"
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
