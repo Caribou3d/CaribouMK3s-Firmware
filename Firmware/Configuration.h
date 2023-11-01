50d071c18c635bed5ad8e824e4baefe1ee830064
wschadow <wolfgang.schadow@t-online.de>
2023-11-01 01:06:56 +0100

    pushed build no

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -28,7 +28,7 @@ extern const char _sPrinterMmuName[] PROGMEM;
     #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION)
 #endif
 
-#define FW_COMMIT_NR 7828
+#define FW_COMMIT_NR 8158
 #define FW_COMMIT_HASH "0b638a1c"
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
